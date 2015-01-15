0802ee1772fbb51ffac4900886c7921cd9ef05f5
Hayley Swimelar <hswimelar@gmail.com>
2015-01-15 07:07:45 -0800

    refactor ruby solution as a method

--- 001_multiples/multiples.rb
+++ 001_multiples/multiples.rb
@@ -1,16 +1,18 @@
 #
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #
-#Find the sum of all the multiples of 3 or 5 below 1000.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 #
-#https://projecteuler.net/problem=1
-#
-#     |Range starting at one and ending before 1000
-#     |         |Accumulator with an ititial value of 0
-#     |         |            |Sets sum to accumulator's initial value (0)
-#     |         |            |    |num is set to the current number in the range
-#     |         |            |    |    |add num to sum
-#     |         |            |    |    |          |but only if num is a multiple of 3 or 5
-#     |         |            |    |    |          |                                 |return sum
-#     v         v            v    v    v          v                                 v
-puts (1...1000).inject(0) { |sum, num| sum += num if num % 3 == 0 or num % 5 == 0 ; sum}
+# https://projecteuler.net/problem=1
+
+def multiples(numbers)
+  numbers.inject(0) do |a, e|
+    # sum the elements into the accumulator
+    # if they are divisible by 3 or 5
+    a += e if e % 3 == 0 || e % 5 == 0
+    a
+  end
+end
+
+puts multiples(1...1000)
