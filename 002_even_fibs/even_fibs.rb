ac75dd12faa92d30214e44895d43698af5bdcc8a
Hayley Swimelar <hswimelar@gmail.com>
2015-01-16 08:25:55 -0800

    add ruby solution to problem 2

--- 002_even_fibs/even_fibs.rb
+++ 002_even_fibs/even_fibs.rb
@@ -7,3 +7,31 @@
 # four million, find the sum of the even-valued terms.
 #
 # https://projecteuler.net/problem=2
+
+# generates fibonacci-like arrays whose members do not exceed a limit
+# user may define first two numbers, standard fibonacci sequence is the default
+def sequence_to(limit, a = 0, b = 1)
+  # initialize array to hold sequence
+  sequence = [a, b]
+  loop do
+    # push the sum of the last two elements of the aray onto the array
+    sequence.push(sequence.last(2).inject(:+))
+    # if the sequence has exceeded the limit break from the loop
+    # and pop the last element
+    break if sequence.last > limit && sequence.pop
+  end
+  # return sequence
+  sequence
+end
+
+def sum_of_even_elements(array)
+  array.inject(0) do |a, e|
+    a += e if e.even?
+    a
+  end
+end
+
+# solution to project euler 2
+puts sum_of_even_elements(sequence_to(4_000_000))
+# lucas numbers variation
+puts sum_of_even_elements(sequence_to(4_000_000, 2, 1))
