15cbd3e49e321b4e3b2914355a43559a9d92e264
Hayley Swimelar <hswimelar@gmail.com>
2015-01-18 22:11:03 -0800

    update comments to javascript solution to problem 3 and clean up whitespace

--- 003_largest_prime_factor/largest_prime_factor.js
+++ 003_largest_prime_factor/largest_prime_factor.js
@@ -3,17 +3,23 @@
 //What is the largest prime factor of the number 600851475143 ?
 //
 //https://projecteuler.net/problem=3
+'use strict';
 
+//test to determine if a number is prime
 function isPrime(num) {
     var i,
-        m = Math.sqrt(num);
-    if (num < 2) { return false;}
-    
+        m = Math.sqrt(num); // sqaure root of number to test
+    if (num < 2) { return false; } // numbers less than 2 are not prime
+
+    // starting at 3, check if every odd number
+    // less than the square root of the number to be tested
+    // is a factor of that number
     for (i = 3; i < m; i += 2) {
-       if (num % i === 0) {
-           return false;
+        if (num % i === 0) {
+            return false;
         }
     }
+    // the number is prime if none of those numbers are a factor
     return true;
 }
 
@@ -22,7 +28,7 @@ function largestPrimeFactorOf(num) {
         m = Math.sqrt(num),
         primeFactorArr = [];
 
-    for (i = 3; i < m; i +=2) {
+    for (i = 3; i < m; i += 2) {
         if (isPrime(i) && num % i === 0) {
             primeFactorArr.push(i);
         }
