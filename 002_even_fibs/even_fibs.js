2039bd3e513d3730474f5a7c06ece30dbf864179
Hayley Swimelar <hswimelar@gmail.com>
2015-01-18 22:02:28 -0800

    update comments to javascript solution to problem 2 and clean up whitespace

--- 002_even_fibs/even_fibs.js
+++ 002_even_fibs/even_fibs.js
@@ -9,18 +9,19 @@
 //
 //https://projecteuler.net/problem=2
 
+'use strict';
 //returns Fibonacci sequence as an array
 function fibsTo(limit) {
-    var a = 0,
+    var a = 0, // a and b are the first to number of the Fibonacci sequence
         b = 1,
         c,
-        arr = [0];  
+        arr = [0];
 
     while (arr[arr.length - 1] <= limit) {   //check if last element in arr is larger than limit
         arr.push(b);
-        c = a + b;
-        a = b;
-        b = c;
+        c = a + b; // c is the sum of the last two number of the sequence
+        a = b;     // a is now the second to last number of the sequence
+        b = c;     // b is now the last member of the sequence
         if (arr[arr.length - 1] > limit) {  //pop last element from array if it's larger than limit and break out of loop
             arr.pop();
             break;
@@ -29,21 +30,23 @@ function fibsTo(limit) {
     return arr;
 }
 
-function evens (arr) {
+// takes and array and returns an array of the even elements in that array
+function evens(arr) {
     var i,
         limit = arr.length,
         evenArr = [];
 
     for (i = 0; i <= limit; i += 1) {
-       if (arr[i] % 2 === 0) {
-           evenArr.push(arr[i]);
-       }
+        if (arr[i] % 2 === 0) {
+            evenArr.push(arr[i]);
+        }
     }
     return evenArr;
 }
 
 var evenFibs = evens(fibsTo(4000000));
 
-console.log(evenFibs.reduce(function(a, b) {
+// sums all the elements in the evenFibs array
+console.log(evenFibs.reduce(function (a, b) {
     return a + b;
 }));
